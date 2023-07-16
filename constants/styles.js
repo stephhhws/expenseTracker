c394cbc3b848b6eaba37bc2c34bf39968a7d52b3
Steph <stepskycc@gmail.com>
2023-07-16 00:59:02 -0400

    first commit

---
+++ constants/styles.js
@@ -0,0 +1,33 @@
+// export const GlobalStyles = {
+//     colors: {
+//       primary50: '#d08c60',
+//       primary100: '#e4b074',
+//       primary200: '#f1dca7',
+//       primary400: '#e5c59e',
+//       primary500: '#d9ae94',
+//       primary700: '#9b9b7a',
+//       primary800: '#797d62',
+//       accent500: '#997b66',
+//       error50: '#fcc4e4',
+//       error500: '#9b095c',
+//       gray500: '#39324a',
+//       gray700: '#221c30',
+//     },
+//   };
+
+  export const GlobalStyles = {
+    colors: {
+      primary50: '#e6ccb2', // beige
+      primary100: '#ede0d4', // lightbeige
+      primary200: '#83978C',// green
+      primary400: '#93939B', //greyblue
+      primary500: '#7A848D', // blue
+      primary700: '#b1c0c4', // light blue
+      primary800: '#e6b8a2', // pink
+      accent500: '#976666', // red
+      error50: '#fcc4e4',
+      error500: '#e9ecef',
+      gray500: '#39324a',
+      gray700: '#221c30',
+    },
+  };
---
+++ firebaseconfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7n-W0EJSIejQOz70_a0Gaw6PBmnsVYj4",
+  authDomain: "tracker-855e5.firebaseapp.com",
+  projectId: "tracker-855e5",
+  storageBucket: "tracker-855e5.appspot.com",
+  messagingSenderId: "250318616101",
+  appId: "1:250318616101:web:2af6712d64bdaeb69d71cc"
+};
+
+// Initialize Firebase
+const FIREBASE_APP = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+
+export { FIRESTORE_DB, FIREBASE_AUTH };
